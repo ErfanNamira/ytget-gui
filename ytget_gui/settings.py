6225caff8ff2a6b8584f4881887ccd0cdd5c3bc3
Namira <115206959+ErfanNamira@users.noreply.github.com>
2026-02-08 20:35:12 +0000

    Update version number to 2.6.0

--- ytget_gui/settings.py
+++ ytget_gui/settings.py
@@ -19,7 +19,7 @@ from ytget_gui.utils.paths import (
 
 @dataclass
 class AppSettings:
-    VERSION: str = "2.5.4"
+    VERSION: str = "2.6.0"
     APP_NAME: str = "YTGet"
     GITHUB_URL: str = "https://github.com/ErfanNamira/ytget-gui"
 
