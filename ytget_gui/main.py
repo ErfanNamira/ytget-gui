c3d05a17622b3f548613832c3f3ddfc3d9fdd547
Namira <115206959+ErfanNamira@users.noreply.github.com>
2026-08-17 22:39:40 +0330

    Bump version from 2.7.7 to 2.7.8

--- ytget_gui/main.py
+++ ytget_gui/main.py
@@ -5,7 +5,7 @@ import sys
 from pathlib import Path
 from platform import system
 
-__version__ = "2.7.7"
+__version__ = "2.7.8"
 
 # --- Windows taskbar icon: set AppUserModelID before QApplication is created ---
 if system() == "Windows":
