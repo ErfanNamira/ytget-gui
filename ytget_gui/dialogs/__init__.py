98ba191e5174d918b8e45a3716e88633c7275c22
Namira <115206959+ErfanNamira@users.noreply.github.com>
2026-08-21 09:08:10 +0330

    Initialize dialogs module with exports
    Added modal dialogs and defined __all__ for exports.

--- ytget_gui/dialogs/__init__.py
+++ ytget_gui/dialogs/__init__.py
@@ -1,2 +1,12 @@
-# File: ytget_gui/__init__.py
-__all__ = []
+# File: ytget_gui/dialogs/__init__.py
+"""Modal dialogs."""
+
+from __future__ import annotations
+
+__all__ = [
+    "about_dialog",
+    "advanced",
+    "preferences",
+    "spotdl_preferences_tab",
+    "update_manager",
+]
