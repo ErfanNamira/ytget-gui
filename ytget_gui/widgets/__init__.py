4a01886dbf89b98ae1073832c0c6e05d7d586c9c
Namira <115206959+ErfanNamira@users.noreply.github.com>
2026-08-21 09:09:26 +0330

    Add UISwitch to module exports

--- ytget_gui/widgets/__init__.py
+++ ytget_gui/widgets/__init__.py
@@ -4,5 +4,6 @@
 from __future__ import annotations
 
 from ytget_gui.widgets.queue_card import QueueCard
+from ytget_gui.widgets.ui_switch import UISwitch
 
-__all__ = ["QueueCard"]
+__all__ = ["QueueCard", "UISwitch"]
